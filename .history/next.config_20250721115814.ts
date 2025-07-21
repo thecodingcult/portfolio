9b9249fafa580dead7ad1ec3a1dd579c146c6248
The Coding Cult <118013483+thecodingcult@users.noreply.github.com>
2025-07-21 12:46:43 -0400

    Fix blog page typing for Next 15

--- .history/next.config_20250721115814.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["www.gstatic.com"], // Allow images from this domain
-  },
-};
-
-export default nextConfig;
